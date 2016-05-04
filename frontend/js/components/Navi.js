53ce6c807b8c9445b3511f6303e55dcd19e6f28c
saito400 <ken.s.530000@gmail.com>
2016-05-04 09:06:43 +0900

    use bootstrap

--- frontend/js/components/Navi.js
+++ frontend/js/components/Navi.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Link, browserHistory } from 'react-router'
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 export default function Navi({ children }) {
   return (
     <div>
-      <header>
-        Links:
-        {' '}
-        <Link to="/">Home</Link>
-        {' '}
-        <Link to="/home">Home</Link>
-        {' '}
-        <Link to="/test">Test</Link>
-      </header>
-      <div>
-        <button onClick={() => browserHistory.push('/test')}>Go to /test</button>
-      </div>
-      <div style={{ marginTop: '1.5em' }}>{children}</div>
+      <nav className='navbar navbar-inverse navbar-fixed-top'>
+        <div id="navbar" className="navbar-collapse collapse">
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/test">Test</Link>
+            </li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div style={{ marginTop: '3em' }}>{children}</div>
     </div>
   )
 }
