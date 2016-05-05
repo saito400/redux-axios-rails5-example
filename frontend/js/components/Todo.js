1d033f25d787598c267f5f0c46ddd9114e727e20
saito400 <ken.s.530000@gmail.com>
2016-05-05 14:46:46 +0900

    add todo

---
+++ frontend/js/components/Todo.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Todo extends Component{
+  constructor(props){
+    super(props);
+    }
+
+    render() {
+      return (
+        <div className='container'>
+          <h2>Todo test</h2>
+        </div>
+      )
+    }
+};
+
+export default Todo;
--- frontend/js/components/index.js
+++ frontend/js/components/index.js
@@ -1,4 +1,5 @@
 export Navi from './Navi'
 export Home from './Home'
 export Test from './Test'
+export Todo from './Todo'
 
