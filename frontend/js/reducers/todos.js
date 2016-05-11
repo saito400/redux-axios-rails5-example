17fea1385281352406613e9eec924cba63df1e52
saito400 <ken.s.530000@gmail.com>
2016-05-11 17:14:49 +0900

    remove unused block

--- frontend/js/reducers/todos.js
+++ frontend/js/reducers/todos.js
@@ -15,22 +15,13 @@ const initialState = [
 
 export default function todos(state = initialState, action) {
   switch (action.type) {
-    case types.ADD_TODO:
-      return [
-        {
-          id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
-          completed: false,
-          text: action.text
-        },
-        ...state
-      ]
-      case types.RECV_ERROR:
-        //TODO do something
-        return state
-      case types.RECV_DATA:
-        return action.data
-      case types.REQ_DATA:
-        return state
+    case types.RECV_ERROR:
+      //TODO do something
+      return state
+    case types.RECV_DATA:
+      return action.data
+    case types.REQ_DATA:
+      return state
     default:
       return state
   }
